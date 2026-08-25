d0597eca9ce64ba8f49f15f12816afc1f42eb9b2
dommie123 <dman9532@gmail.com>
2026-08-25 15:15:50 -0600

    Added part of HUD mockup

---
+++ scripts/hud.gd
@@ -0,0 +1,22 @@
+extends Control
+
+signal make_sale();
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_inventory_button_pressed() -> void:
+	# TODO open store inventory
+	pass
+
+
+func _on_sale_button_pressed() -> void:
+	# TODO open cash register screen
+	pass 
